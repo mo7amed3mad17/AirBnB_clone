b1195e101e02320a24386a6b3d00fb16ddae5072
mo7amed3mad17 <memo12317emad@gmail.com>
2024-03-10 22:53:55 +0200

    Update

--- console.py
+++ console.py
@@ -1,169 +1,135 @@
-# Command interpreter in Python
-
+#!/usr/bin/python3
+""" File of The Console program """
 import cmd
-import models
+from shlex import split
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 from models import storage
 
+
 class HBNBCommand(cmd.Cmd):
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
+    all_classes = {"BaseModel"}
 
     def do_create(self, arg):
-        """Creates a new instance of BaseModel, saves it (to the JSON file) and         prints the id"""
+        """Creates a new instance of BaseModel,
+        saves it (to the JSON file) and
+        prints the id"""
+        tokens = arg.split()
+        class_name = tokens[0]
         if not arg:
             print("** class name missing **")
             return
-
-        class_name = arg.split()[0]
-        all_classes = storage.all()
-        if class_name not in all_classes:
+        elif class_name not in HBNBCommand.all_classes:
             print("** class doesn't exist **")
-            return
+        else:
+            print(eval(class_name)().id)
+            storage.save()
 
-        new_instance = all_classes[class_name]()
-        new_instance.save()
-        print(new_instance.id)
     def do_show(self, line):
         """Shows an instance of a model"""
         tokens = line.split()
+        obj_dict = storage.all()
+
         if len(tokens) == 0:
             print("** class name missing **")
             return
-
-        cls_name = tokens[0]
-        if not hasattr(models, cls_name):
+        elif tokens[0] not in HBNBCommand.all_classes:
             print(f"** class doesn't exist **")
             return
-
-        if len(tokens) == 1:
+        elif len(tokens) == 1:
             print("** instance id missing **")
             return
-
-        id = tokens[1]
-        instances = cls_name.all()
-        instance = instances.get(id)
-        if instance is None:
+        elif "{}.{}".format(tokens[0], tokens[1]) not in obj_dict:
             print("** no instance found **")
         else:
-            print(instance)
-
+            print(obj_dict["{}.{}".format(tokens[0], tokens[1])])
 
     def do_destroy(self, line):
         """Destroys an instance of a model"""
         tokens = line.split()
+        obj_dict = storage.all()
+
         if len(tokens) == 0:
             print("** class name missing **")
-        return
-
-        cls_name = tokens[0]
-        if not hasattr(models, cls_name):
-            print(f"** class doesn't exist **")
-        return
-
-        if len(tokens) == 1:
+            return
+        elif tokens[0] not in HBNBCommand.all_classes:
+            print("** class doesn't exist **")
+            return
+        elif len(tokens) == 1:
             print("** instance id missing **")
-        return
-
-        id = tokens[1]
-        instances = cls_name.all()
-        instance = instances.get(id)
-        if instance is None:
+            return
+        elif "{}.{}".format(tokens[0], tokens[1]) not in obj_dict:
             print("** no instance found **")
         else:
-            instances.delete(instance)
-            instances.save()
-            print(f"** {instance.__class__.__name__} {instance.id} deleted **")
-    
+            del obj_dict["{}.{}".format(tokens[0], tokens[1])]
+            storage.save()
+
     def do_all(self, line):
         """Prints all instances of a model"""
         tokens = line.split()
-        cls_name = tokens[0] if tokens else None
 
-        if cls_name and not hasattr(models, cls_name):
-            print(f"** class doesn't exist **")
-        return
-
-        if cls_name:
-            instances = getattr(models, cls_name).all()
+        if len(tokens) > 0 and tokens[0] not in HBNBCommand.all_classes:
+            print("** class doesn't exist **")
+            return
         else:
-            instances = []
-        for cls in models.__dict__.values():
-            if isinstance(cls, type) and issubclass(cls, BaseModel):
-                class_instances = cls.all()
-                instances.extend(class_instances)
+            obj_list = []
+            for obj in storage.all().values():
+                if len(tokens) > 0 and tokens[0] == obj.__class__.__name__:
+                    obj_list.append(obj.__str__())
+                elif len(tokens) == 0:
+                    obj_list.append(obj.__str__())
+            print(obj_list)
 
-        if not instances:
-            print("** No instances found **")
-        else:
-            print("[")
-            for instance in instances:
-                print(f"  {instance}", end=",")
-            print("]")
-
-    
     def do_update(self, line):
-        """Updates an instance of a model"""
         tokens = line.split()
-        if len(tokens) < 4:
-            if len(tokens) == 0:
-                print("** class name missing **")
-            elif len(tokens) == 1:
-                print("** instance id missing **")
-            elif len(tokens) == 2:
-                print("** attribute name missing **")
-            else:
-                print("** value missing **")
-            return
+        obj_dict = storage.all()
 
-        cls_name = tokens[0]
-        if not hasattr(models, cls_name):
-            print(f"** class doesn't exist **")
-            return
-
-        id = tokens[1]
-        instances = cls_name.all()
-        instance = instances.get(id)
-        if instance is None:
+        if len(tokens) == 0:
+            print("** class name missing **")
+            return False
+        if tokens[0] not in HBNBCommand.all_classes:
+            print("** class doesn't exist **")
+            return False
+        if len(tokens) == 1:
+            print("** instance id missing **")
+            return False
+        if "{}.{}".format(tokens[0], tokens[1]) not in obj_dict.keys():
             print("** no instance found **")
-            return
+            return False
+        if len(tokens) == 2:
+            print("** attribute name missing **")
+            return False
+        if len(tokens) == 3:
+            try:
+                type(eval(tokens[2])) != dict
+            except NameError:
+                print("** value missing **")
+                return False
 
-        attr_name = tokens[2]
-        if attr_name in ["id", "created_at", "updated_at"]:
-            print(f"** {attr_name} cannot be updated **")
-            return
+        instance_key = "{}.{}".format(tokens[0], tokens[1])
+        instance = obj_dict[instance_key]
 
-        attr_value = " ".join(tokens[3:])
-        if attr_value == "":
-            print("** value missing **")
-            return
-
-
-        attr_type = type(getattr(instance, attr_name))
-        if attr_type == str:
-            new_value = attr_value
-        elif attr_type == int:
-            new_value = int(attr_value)
-        elif attr_type == float:
-            new_value = float(attr_value)
-        else:
-            print("** Only simple attributes (str, int, float) can be updated **")
-            return
-
-        setattr(instance, attr_name, new_value)
-        instances.save()
-        print(f"** {instance.__class__.__name__} {instance.id} updated **")
-
+        if len(tokens) == 4:
+            attribute_name = tokens[2]
+            attribute_value = tokens[3]
+            setattr(instance, attribute_name, attribute_value)
+        elif isinstance(eval(tokens[2]), dict):
+            attribute_dict = eval(tokens[2])
+            for k, v in attribute_dict.items():
+                setattr(instance, k, v)
+        storage.save()
 
     def do_quit(self, arg):
         """This quits or exits the program"""
         return True
 
-    # Aliasing
     do_EOF = do_quit
 
     def emptyline(self):
         """Called when an empty line is entered."""
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
